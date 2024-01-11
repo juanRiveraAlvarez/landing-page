125cba1bcd686dedb2af307ca3892a14e1743108
juanRiveraAlvarez <juanpariveraalvarez@gmail.com>
2024-01-10 19:05:34 -0500

    Templates

---
+++ templates/test/script.js
@@ -0,0 +1,7 @@
+document.getElementById('formulario').addEventListener('submit', function(event) {
+  event.preventDefault(); // Evitar que el formulario se envíe de la manera convencional
+
+  // Aquí puedes agregar código para procesar el formulario (enviar datos a un servidor, por ejemplo)
+  alert('Formulario enviado');
+});
+
